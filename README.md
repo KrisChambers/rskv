dd2204cd3f1685b6352e4efa3c713d7f999e9d73
Kris Chambers <kris.chambers@outlook.com>
2026-07-04 17:47:35 -0400

    Implement Basic web api with tokio

---
+++ README.md
@@ -0,0 +1,10 @@
+# Basic Key-Value store over TCP
+
+We will support operations
+
+SET key value
+GET key
+PUB channel message
+SUB channel
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt}, net::{TcpListener, TcpStream},
+};
+
+fn get_addr() -> String {
+    "0.0.0.0:6666".to_string()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let addr = get_addr();
+
+    let listener = TcpListener::bind(&addr).await?;
+    println!("Listening at {addr}");
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        println!("Accepting connection");
+
+        tokio::spawn(async move {
+            echo(&mut socket).await.expect("Failed to echo request")
+        });
+    }
+}
+
+async fn echo(socket: &mut TcpStream) -> Result<(), Box<dyn Error>>{
+    let mut buf = vec![0; 1024];
+
+    loop {
+        let n = socket.read(&mut buf).await?;
+        println!("Read {n} bytes");
+
+        if n == 0 { return Ok(()); };
+
+        socket.write_all(&buf[0..n]).await?;
+    }
 }
